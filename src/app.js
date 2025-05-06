2cd8fedb5486e4cda9555b57ba3a46caa6272d76
unknown <marceloribeirobarbosa92@gmail.com>
2025-05-06 11:18:44 -0300

    Add requisição GET para receber itens

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 import express, {json} from "express"
 
 const app = express();
+
+app.get("/items", (req, res)=>{
+    const itens = [
+        {
+            id:1,
+            name: "maçã",
+            quantity: 2,
+            type:"fruta"
+
+        },
+        {
+            id:2,
+            name: "peito de frango congelado",
+            quantity:1,
+            type:"carne"
+        }
+    ]
+    res.send(itens)
+})
+
+
+
 app.listen(5000, ()=>{
-    console.log("Servidor está disponível na porta 5000")
+    console.log("Servidor está online na porta 5000")
 });
