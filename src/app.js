0e12a356fa2c0c9279948b62c570c393dd73eaf4
unknown <marceloribeirobarbosa92@gmail.com>
2025-05-06 11:41:34 -0300

    Add parametros e variaveis globais

--- src/app.js
+++ src/app.js
@@ -1,24 +1,35 @@
-import express, {json} from "express"
+import express, {json} from "express";
+
 
 const app = express();
 
-app.get("/items", (req, res)=>{
-    const itens = [
-        {
-            id:1,
-            name: "maçã",
-            quantity: 2,
-            type:"fruta"
+const itens = [
+    {
+        id:1,
+        name: "maçã",
+        quantity: 2,
+        type:"fruta"
 
-        },
-        {
-            id:2,
-            name: "peito de frango congelado",
-            quantity:1,
-            type:"carne"
-        }
-    ]
+    },
+    {
+        id:2,
+        name: "peito de frango congelado",
+        quantity:1,
+        type:"carne"
+    }
+]
+
+
+app.get("/items", (req, res)=>{
     res.send(itens)
+});
+
+app.get("/items/:id", (req, res)=>{
+    const id = req.params.id;
+    const produto = itens.find(produto => {
+        return produto.id === Number(id);
+    })
+    res.send(produto);
 })
 
 
